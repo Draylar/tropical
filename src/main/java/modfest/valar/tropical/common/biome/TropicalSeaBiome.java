557e36ca252014015ead2722109de6dea81d173d
Valoeghese <valoeghesemods@gmail.com>
2019-04-20 16:29:06 +1200

    Biome

---
+++ src/main/java/modfest/valar/tropical/common/biome/TropicalSeaBiome.java
@@ -0,0 +1,13 @@
+package modfest.valar.tropical.common.biome;
+
+import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
+
+public class TropicalSeaBiome extends TropicsBiome
+{
+
+	public TropicalSeaBiome()
+	{
+		super(new Properties("tropical_sea", SurfaceBuilder.DEFAULT).withDownfall(Downfall.OCEANIC));
+	}
+
+}
--- src/main/java/modfest/valar/tropical/common/biome/TropicsBiome.java
+++ src/main/java/modfest/valar/tropical/common/biome/TropicsBiome.java
@@ -1,7 +1,8 @@
 package modfest.valar.tropical.common.biome;
 
-import modfest.valar.tropical.TropicalMod;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilder.TernarySurfaceConfig;
 
 public abstract class TropicsBiome extends Biome
 {
@@ -21,24 +22,51 @@ public abstract class TropicsBiome extends Biome
 
 	public static class Properties
 	{
-		private final float baseHeight, scale;
 		private final String name;
+		private final SurfaceBuilder<TernarySurfaceConfig> builder;
+		private Downfall downfall = Downfall.STANDARD;
 		
-		public Properties(String biomeName, float baseHeight, float scale)
+		public Properties(String biomeName, SurfaceBuilder<TernarySurfaceConfig> surfaceBuilder)
 		{
-			this.baseHeight = baseHeight;
-			this.scale = scale;
 			this.name = biomeName;
+			this.builder = surfaceBuilder;
+		}
+		
+		public Properties withDownfall(Downfall downfall)
+		{
+			this.downfall = downfall;
+			
+			return this;
 		}
 		
 		public Biome.Settings build()
 		{
-			return new Biome.Settings().depth(baseHeight).scale(scale);
+			return new Biome.Settings().configureSurfaceBuilder(this.builder, SurfaceBuilder.GRASS_SAND_UNDERWATER_CONFIG).depth(0.0F).scale(0.0F).temperature(1.2F).downfall(this.downfall.toFloat()).waterColor(4445678).waterFogColor(270131).parent((String)null);
 		}
 		
 		public String getId()
 		{
 			return "tropical" + ":" + this.name;
+		}
+	}
+	
+	public static enum Downfall
+	{
+		VULCAN(0.28F),
+		OCEANIC(0.5F),
+		STANDARD(0.78F),
+		HUMID(0.98F);
+		
+		private final float downfall;
+		
+		private Downfall(float downfall)
+		{
+			this.downfall = downfall;
+		}
+		
+		public float toFloat()
+		{
+			return this.downfall;
 		}
 	}
 
