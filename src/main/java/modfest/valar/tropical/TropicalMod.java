db055746a18dd54059cf390a39dbcbb03775286a
Draylar <samuelhimes@gmail.com>
2019-04-19 22:32:45 -0500

    Added registry system for items & blocks & fixed gitignore

--- src/main/java/modfest/valar/tropical/TropicalMod.java
+++ src/main/java/modfest/valar/tropical/TropicalMod.java
@@ -1,10 +1,18 @@
 package modfest.valar.tropical;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
 
-public class TropicalMod implements ModInitializer {
+public class TropicalMod implements ModInitializer
+{
 	@Override
-	public void onInitialize() {
+	public void onInitialize()
+	{
 
+	}
+
+	public static Identifier getModIdentifier(String name)
+	{
+		return new Identifier("tropical", name);
 	}
 }
