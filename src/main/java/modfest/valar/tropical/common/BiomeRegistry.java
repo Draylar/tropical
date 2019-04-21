fc33ee6728fada438a0968e6cb6b4914dfd78b62
Valoeghese <valoeghesemods@gmail.com>
2019-04-21 12:07:47 +1200

    asie's 'unnecessary code' fixes

--- src/main/java/modfest/valar/tropical/common/BiomeRegistry.java
+++ src/main/java/modfest/valar/tropical/common/BiomeRegistry.java
@@ -1,47 +1,25 @@
 package modfest.valar.tropical.common;
 
 import modfest.valar.tropical.common.biome.TropicsBiome;
-import modfest.valar.tropical.common.biome.TropicsBiome;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 
 public class BiomeRegistry
 {
-	private static int nextID = 0;
 	
 	//Mostly just to initialise MC biomes before custom biomes
 	public static final Biome defaultBiome = Biomes.PLAINS;
 	
-	private static Biome register(Biome biome, String ID, int specifiedID)
+	private static Biome register(Biome biome, String ID)
 	{	
-		Registry.register(Registry.BIOME, specifiedID, ID, biome);
-		if (biome.hasParent())
-		{
-			Biome.PARENT_BIOME_ID_MAP.set(biome, Registry.BIOME.getRawId(Registry.BIOME.get(new Identifier(biome.getParent()))));
-		}
+		Registry.register(Registry.BIOME, ID, biome);
 
 		return biome;
 	}
 	
 	public static Biome register(TropicsBiome biome)
 	{
-		int thisId = getNextId();
-		
-		return register(biome, biome.getId(), thisId);
-	}
-	
-	private static int getNextId()
-	{
-		++nextID;
-		while (isIdTaken(nextID)) ++nextID;
-		
-		return nextID;
-	}
-	
-	private static boolean isIdTaken(int id)
-	{
-		return !(Registry.BIOME.get(id) == null);
+		return register(biome, biome.getId());
 	}
 }
