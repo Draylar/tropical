99381fc25c6ea30f9a1ba89587d99bc3286a64eb
Draylar <samuelhimes@gmail.com>
2019-07-03 18:41:13 -0500

    refactored blocks

--- src/main/java/modfest/valar/tropical/common/TropicalBlocks.java
+++ src/main/java/modfest/valar/tropical/common/TropicalBlocks.java
@@ -16,40 +16,18 @@ import net.minecraft.util.registry.Registry;
 
 public class TropicalBlocks
 {
-    public static MargaritaMakerBlock MARGARITA_MAKER = new MargaritaMakerBlock(FabricBlockSettings.of(Material.STONE).build());
-    public static PineappleBlock PINEAPPLE = new PineappleBlock(FabricBlockSettings.of(Material.PUMPKIN).build());
-    public static CustomSandBlock WHITE_SAND = new CustomSandBlock(FabricBlockSettings.of(Material.SAND).sounds(BlockSoundGroup.SAND).hardness(0.1F).resistance(0.1F).build());
+    public static Block MARGARITA_MAKER = register("margarita_maker", new MargaritaMakerBlock(FabricBlockSettings.of(Material.STONE).build()));
+    public static Block PINEAPPLE = register("pineapple", new PineappleBlock(FabricBlockSettings.of(Material.PUMPKIN).build()));
+    public static Block WHITE_SAND = register("white_sand", new CustomSandBlock(FabricBlockSettings.of(Material.SAND).sounds(BlockSoundGroup.SAND).hardness(0.1F).resistance(0.1F).build()));
+    public static Block BASALT = register("basalt", new Block(FabricBlockSettings.of(Material.STONE).hardness(1f).build()));
     
     public static void register()
     {
-        register(MARGARITA_MAKER, "margarita_maker");
-        register(PINEAPPLE, "pineapple");
-        register(WHITE_SAND, "white_sand").registerItemBlock(TropicalMod.GROUP);;
+
     }
     
-    private static BlockRegistryOutput register(Block block, String name)
+    private static Block register(String name, Block block)
     {
-    	Identifier identifier = TropicalMod.getModIdentifier(name);
-    	
-        Registry.register(Registry.BLOCK, identifier, block);
-        
-        return new BlockRegistryOutput(block, identifier);
-    }
-    
-    private static final class BlockRegistryOutput
-    {
-    	private final Block block;
-    	private final Identifier name;
-    	
-    	private BlockRegistryOutput(Block block, Identifier name)
-    	{
-    		this.block = block;
-    		this.name = name;
-    	}
-    	
-    	public void registerItemBlock(ItemGroup group)
-    	{
-    		Registry.register(Registry.ITEM, this.name, new BlockItem(this.block, new Item.Settings().group(group)));
-    	}
+        return Registry.register(Registry.BLOCK, new Identifier("tropical", name), block);
     }
 }
